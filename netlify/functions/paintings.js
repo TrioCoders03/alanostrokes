f87b6aa47badfc4912f61c77b93b9cff2cfeb78b
JRK <jrk@JRKs-MacBook-Pro.local>
2026-04-28 00:32:09 +1000

    Sort paintings by createdTime in function instead of API param

--- netlify/functions/paintings.js
+++ netlify/functions/paintings.js
@@ -1,10 +1,11 @@
 exports.handler = async function () {
   const res = await fetch(
-    'https://api.airtable.com/v0/app3PaRCny9Mjr5I2/tblLUOPyOSF1CDUb8?sort[0][field]=Created+Time&sort[0][direction]=asc',
+    'https://api.airtable.com/v0/app3PaRCny9Mjr5I2/tblLUOPyOSF1CDUb8',
     { headers: { Authorization: 'Bearer ' + process.env.AIRTABLE_TOKEN } }
   );
   const data = await res.json();
-  const records = data.records.map((r, i) => ({
+  const sorted = data.records.sort((a, b) => new Date(a.createdTime) - new Date(b.createdTime));
+  const records = sorted.map((r, i) => ({
     id: i + 1,
     title: r.fields.Title,
     category: r.fields.Catagory,
